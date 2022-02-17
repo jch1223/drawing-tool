7d51cc37491a150d51f2315bf213a5942b6bfceb
jch1223 <ico1828@gmail.com>
2022-02-17 18:45:19 +0900

    feat: react-konva tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Stage, Layer, Line } from "react-konva";
 
 function App() {
+  const [tool, setTool] = useState("pen");
+  const [lines, setLines] = useState([
+    {
+      tool: "pen",
+      points: [127, 236, 173, 372, 409, 230],
+    },
+  ]);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const onMouseDownHandler = (e) => {
+    const { x, y } = e.target.getStage().getPointerPosition();
+
+    setIsDrawing(true);
+    setLines([...lines, { tool, points: [x, y] }]);
+  };
+  console.log(lines);
+  const onMouseMoveHandler = (e) => {
+    if (!isDrawing) return;
+
+    const newLines = [...lines];
+    const lastLine = newLines[newLines.length - 1];
+    const { x, y } = e.target.getStage().getPointerPosition();
+
+    lastLine.points = [...lastLine.points, x, y];
+    newLines[newLines.length - 1] = lastLine;
+
+    setLines(newLines);
+  };
+
+  const onMouseUpHandler = () => {
+    setIsDrawing(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={onMouseDownHandler}
+        onMouseMove={onMouseMoveHandler}
+        onMouseUp={onMouseUpHandler}
+      >
+        <Layer>
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="#df4b26"
+              strokeWidth={10}
+              tension={1}
+              lineCap="round"
+              lineJoin="round"
+              globalCompositeOperation={
+                line.tool === "eraser" ? "destination-out" : "source-over"
+              }
+            />
+          ))}
+        </Layer>
+      </Stage>
     </div>
   );
 }
